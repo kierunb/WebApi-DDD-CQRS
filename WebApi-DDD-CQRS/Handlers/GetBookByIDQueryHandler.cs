2573065426ad8f5a0ed929bfcbbde9ac2459966e
kierunb <kierunb@windowslive.com>
2019-11-26 13:21:06 +0100

    automapper 1

--- WebApi-DDD-CQRS/Handlers/GetBookByIDQueryHandler.cs
+++ WebApi-DDD-CQRS/Handlers/GetBookByIDQueryHandler.cs
@@ -4,9 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
+using AutoMapper.QueryableExtensions;
 using MediatR;
 using Microsoft.EntityFrameworkCore;
 using WebApi_DDD_CQRS.DbContexts;
+using WebApi_DDD_CQRS.Repositories;
 using WebApi_DDD_CQRS.ResponseModels;
 
 
@@ -31,10 +34,12 @@ namespace WebApi_DDD_CQRS.Handlers
     public class GetBookByIDRequestHandler : IRequestHandler<GetBookByIDRequest, GetBookByIDResponseModel>
     {
         private readonly BooksDbContext db;
+        private readonly IConfigurationProvider configuration;
 
-        public GetBookByIDRequestHandler(BooksDbContext db)
+        public GetBookByIDRequestHandler(BooksDbContext db, IConfigurationProvider configuration)
         {
             this.db = db;
+            this.configuration = configuration;
         }
         
         public async Task<GetBookByIDResponseModel> Handle(GetBookByIDRequest request, 
@@ -43,19 +48,32 @@ namespace WebApi_DDD_CQRS.Handlers
             // pobierzemy dokladnie te dane ktore trzeba
             // i zmapujemy na model wyjsciowy
 
+            //var response = await db.Books
+            //    .Select(b => new GetBookByIDResponseModel{ 
+            //        BookId = b.BookId,
+            //        Description = b.Description,
+            //        Language = b.Language,
+            //        Title = b.Title,
+            //        PublishedOn = b.PublishedOn,
+            //        Publisher = b.Publisher
+            //    })
+            //    .FirstOrDefaultAsync(b => b.BookId == request.BookId);
+
             var response = await db.Books
-                .Select(b => new GetBookByIDResponseModel{ 
-                    BookId = b.BookId,
-                    Description = b.Description,
-                    Language = b.Language,
-                    Title = b.Title,
-                    PublishedOn = b.PublishedOn,
-                    Publisher = b.Publisher
-                })
+                .ProjectTo<GetBookByIDResponseModel>(this.configuration)
                 .FirstOrDefaultAsync(b => b.BookId == request.BookId);
-                
-            
             return response;
+        }
+    }
+
+
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<BookRequestModel, Book>();
+            CreateMap<Book, GetBookByIDResponseModel>();
+
         }
     }
 }
